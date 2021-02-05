40b4a31eeb7728681771200ebc85e7d6fd3903df
danielmwangiwanjiku <77952543+danielmwangiwanjiku@users.noreply.github.com>
2021-02-05 10:44:20 +0300

    Add files via upload

---
+++ VENUS.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package venus;
+
+import login.login;
+import reg.registration;
+
+/**
+ *
+ * @author Admin
+ */
+public class VENUS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     new registration().setVisible(true);         
+    }
+    
+}
